c5638ebb37ec5dc284376fd8d9f78d81e8120291
kalkikaruna <kalkikaruna03@gmail.com>
2024-09-29 11:29:19 +0530

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -6,23 +6,17 @@ const MongoClient = require('mongodb').MongoClient;
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
+const dbName = "secondChance";
 
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    const db = await connectToDatabase();
-   
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-    const dbInstance = db.connect('giftDB');
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    await client.connect();
+    dbInstance = client.db(dbName);
     return dbInstance;
 }
 
